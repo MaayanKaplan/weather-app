48fd1f344c6ec0bdc0c0536139905e5dc9b13364
MaayanKaplan <maayanmanor25@gmail.com>
2022-07-13 20:31:34 +0300

    first-changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import GlobalStyles from "./GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import theme from "./themes";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ src/themes.tsx
@@ -0,0 +1,19 @@
+const lightModeTheme = {
+  black: "#222",
+  white: "#fff",
+  primary: "#48bae4",
+  secondary: "#444e72",
+  fadeText: "#838BAA",
+  gray: "#BEBEBE",
+  grayBg: "#F2F2F2",
+  errorRed: "#F0274B",
+  yellow: "#FFD130",
+};
+
+const darkMode = {
+  primary: "#1a2b55",
+};
+
+const darkModeTheme = { ...lightModeTheme, ...darkMode };
+
+export default lightModeTheme;
